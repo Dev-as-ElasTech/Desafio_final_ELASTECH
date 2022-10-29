f6054e0427180322db33017ef4d3c7c348b13b16
brunahirano <bruna_hi@hotmail.com>
2022-10-29 11:07:52 -0300

    Projeto inicial com Spring Boot

---
+++ src/main/java/br/com/divasbank/DivasbankApplication.java
@@ -0,0 +1,13 @@
+package br.com.divasbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivasbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivasbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/divasbank/DivasbankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.divasbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivasbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
