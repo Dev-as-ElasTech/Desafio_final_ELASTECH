6f77d92465dd2701c302acf8e6c58711ef9698bd
brunahirano <bruna_hi@hotmail.com>
2022-11-08 21:20:16 -0300

    Adicionando métodos nos Services.

---
+++ src/main/java/br/com/divasbank/service/ContaService.java
@@ -0,0 +1,29 @@
+package br.com.divasbank.service;
+
+import br.com.divasbank.model.Conta;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.List;
+
+@Component
+public class ContaService {
+
+    public void listarTodos() {
+
+    }
+
+    public void listarPorId() {
+
+    }
+
+    public void cadastrar() {
+
+    }
+
+    public void inativar() {
+
+    }
+}
---
+++ src/main/java/br/com/divasbank/service/EnderecoService.java
@@ -0,0 +1,38 @@
+package br.com.divasbank.service;
+
+import br.com.divasbank.model.Endereco;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.List;
+
+@Component
+public class EnderecoService {
+
+    public void listarTodos() {
+
+    }
+
+    public void listarEnderecoPorId() {
+
+    }
+
+    public void listarEnderecoPorCliente() {
+
+    }
+
+    public void cadastrar() {
+
+    }
+
+    public void editar() {
+
+    }
+
+    public void deletar() {
+
+    }
+}
