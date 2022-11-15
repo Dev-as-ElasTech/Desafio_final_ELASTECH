b18d2f94c9a3ceaa25f6e1a281da39e1a0f0ecf6
brunahirano <bruna_hi@hotmail.com>
2022-11-14 22:53:38 -0300

    Criando os métodos no ClienteService.

--- src/main/java/br/com/divasbank/service/ClienteService.java
+++ src/main/java/br/com/divasbank/service/ClienteService.java
@@ -1,8 +1,12 @@
 package br.com.divasbank.service;
 
+import br.com.divasbank.model.Cliente;
 import br.com.divasbank.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class ClienteService {
@@ -10,25 +14,27 @@ public class ClienteService {
     @Autowired
     ClienteRepository clienteRepository;
 
-    public void listarTodos() {
-
+    public List<Cliente> listarTodos() {
+        return clienteRepository.findAll();
     }
 
-    public void listarPorId() {
-
+    public Optional<Cliente> listarClientePorId(Long id) {
+        return clienteRepository.findById(id);
     }
 
-    public void cadastrar() {
-
+    public Cliente cadastrar(Cliente cliente) {
+        return clienteRepository.save(cliente);
     }
 
-    public void editar() {
-
+    public Cliente editar(Cliente cliente) {
+        listarClientePorId(cliente.getId());
+        return clienteRepository.save(cliente);
     }
 
-    public void inativar() {
-
+    public Cliente inativar(Cliente cliente) {
+        listarClientePorId(cliente.getId());
+        cliente.setAtivo(false);
+        return clienteRepository.save(cliente);
     }
-
 
 }
