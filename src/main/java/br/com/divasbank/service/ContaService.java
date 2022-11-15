555d29a0745726bd31368a795c4a7efc9d20c2f3
luiza <luizatrocino@hotmail.com>
2022-11-15 18:14:25 -0300

    Atualizando o ContaService, inserindo os métodos do CRUD.

--- src/main/java/br/com/divasbank/service/ContaService.java
+++ src/main/java/br/com/divasbank/service/ContaService.java
@@ -1,8 +1,12 @@
 package br.com.divasbank.service;
 
+import br.com.divasbank.model.Conta;
 import br.com.divasbank.repository.ContaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class ContaService {
@@ -10,19 +14,21 @@ public class ContaService {
     @Autowired
     ContaRepository contaRepository;
 
-    public void listarTodos() {
-
+    public List<Conta> listarTodos() {
+        return contaRepository.findAll();
     }
 
-    public void listarPorId() {
-
+    public Optional<Conta> listarPorId(Long id) {
+       return contaRepository.findById(id);
     }
 
-    public void cadastrar() {
-
+    public Conta cadastrar(Conta conta) {
+       return contaRepository.save(conta);
     }
 
-    public void inativar() {
-
+    public Conta inativar(Conta conta) {
+        listarPorId(conta.getId());
+        conta.setAtivo(false);
+        return contaRepository.save(conta);
     }
 }
